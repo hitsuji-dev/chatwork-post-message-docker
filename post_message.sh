293075b4fd3c4730452b5a7953947ef275737cab
hitsuji-dev <3807845+hitsuji-dev@users.noreply.github.com>
2020-01-23 21:30:39 +0900

    Update post_message.sh

--- post_message.sh
+++ post_message.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -sS -X POST -H "X-ChatWorkToken: ${CW_TOKEN}" -d "body=${1}&self_unread=0" "https://api.chatwork.com/v2/rooms/${CW_ROOM_ID}/messages" > /dev/null
+curl -sS -X POST -H "X-ChatWorkToken: ${CW_TOKEN}" -d "body=${1}&self_unread=0" "https://api.chatwork.com/v2/rooms/${CW_ROOM_ID}/messages"
